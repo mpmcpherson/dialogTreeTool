e0e43410c5288acd462b17027e58421e8103c74d
Michael McPherson <michael@mpmcpherson.com>
2021-05-04 01:54:18 -0400

    still nauseous. Getting some resizing done.

---
+++ site/app.js
@@ -0,0 +1,32 @@
+window.onload - function(){
+
+	window.addEventListener('resize', function(event) {
+
+
+		let elements = getElementsStartsWithId("GridElement");
+
+		elements.foreach(resize);
+	}, true);
+
+
+};
+
+function resize(item, index, arr){
+	let height = document.body.clientHeight;
+	let width = document.body.clientWidth;
+
+	arr[index].style.height = (height/3)+".px";
+	arr[index].style.width = (width/3)+".px";
+
+}
+
+function getElementsStartsWithId( id ) {
+  let children = document.body.getElementsByTagName('*');
+  let elements = [], child;
+  for (var i = 0, length = children.length; i < length; i++) {
+    child = children[i];
+    if (child.id.substr(0, id.length) == id)
+      elements.push(child);
+  }
+  return elements;
+}
--- site/index.php
+++ site/index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="css\style.css">
-		<link rel="stylesheet" href="css\bootstrap.css">
+		
 
 	</head>
 	<body style="background-color: gray;">
@@ -12,16 +12,16 @@
 		$cols=3;
 		$rows=3;
 		
-		$maxCol=12;
-		$colSum=$maxCol/$cols;
+		$maxCol=100;
+		$colSum=round($maxCol/$cols,0);
 
 		$percentheight = 100;
-		$maxRow=round($percentheight/$rows,2);
+		$maxRow=round($percentheight/$rows,0);
 
 		for($i=0;$i<$cols;$i++){
 			echo "<div id='row".$i."' style='width: 100%; overflow-x: scroll;'>";
 			for($j=0;$j<$rows;$j++){
-				echo "<div id='".$i."By".$j."' style='height: ".$maxRow."%; margin: 5px;' class='col-lg-$colSum'></div>";
+				echo "<div id='GridElement".$i."By".$j."' style='height: ".$maxRow."%; width: ".$colSum."%; display: inline-block;' class=''>element</div>";
 			}	
 			echo "</div>";
 		}
@@ -29,5 +29,5 @@
 		</div>
 	</body>
 </html>
-<script type="text/javascript" src="js\bootstrap.bundle.js"></script>
+
 <script type="text/javascript" src="js\app.js"></script>
