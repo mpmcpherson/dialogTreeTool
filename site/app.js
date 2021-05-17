6dcb2c3f04497e96fc66a6c8360b5d45699b7037
Michael McPherson <michael@mpmcpherson.com>
2021-05-17 04:05:58 -0400

    autocommit Mon 17 May 2021 04:05:58 AM EDT

--- site/app.js
+++ site/app.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
-
 	window.addEventListener('resize', function() {
-
-
 		let elements = getElementsStartsWithId("GridElement");
 		console.log(elements);
 		elements.foreach(resize);
-
-
-
 	}, true);
 
 	let container = $("#GridElement_container");
@@ -25,10 +19,8 @@ $(document).ready(function(){
 	let yRatio=Math.round(myHeight/y);
 
 	for(let i=1;i<10;i++){
-		container.append("<div style='width: "+xRatio+"px; height: "+yRatio+"px; display: inline-flex;'><h1>"+i+"</h1></div>");
-
+		container.append("<div style='width: "+xRatio+"px; height: "+yRatio+"px; display: inline-flex; ondrop='drop(event)' ondragover='allowDrop(event)'><h1>"+i+"</h1></div>");
 	}
-
 });
 
 function resize(item, index, arr){
@@ -37,7 +29,6 @@ function resize(item, index, arr){
 
 	arr[index].style.height = (height/3)+".px";
 	arr[index].style.width = (width/3)+".px";
-
 }
 
 function getElementsStartsWithId( id ) {
@@ -50,4 +41,24 @@ function getElementsStartsWithId( id ) {
     }
   }
   return elements;
+}
+
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var appDiv = document.getElementById(data);
+    ev.target.appendChild(appDiv);
+
+    appDiv.style = "display: inline-flex; margin: 3px; cursor: grabbing; border-radius: 5px;";
+    
+    
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
 }
